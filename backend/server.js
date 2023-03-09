6c67a90a53dbc0b3328591816d7737c342c69c05
bennizri <bennizri22@gmail.com>
2023-03-09 13:19:56 +0200

    socket

--- backend/server.js
+++ backend/server.js
@@ -1,28 +1,48 @@
-import express from 'express';
-import data from './data.js';
-import mongoose from 'mongoose';
-import seedRouter from './routes/seedRoutes.js';
-import productRouter from './routes/productRoutes.js';
-import shoppingRouter from './routes/shoppingRouter.js'
-import bodyParser from 'body-parser';
-import cors from 'cors';
+import data from "./data.js";
+import mongoose from "mongoose";
+import seedRouter from "./routes/seedRoutes.js";
+import productRouter from "./routes/productRoutes.js";
+import shoppingRouter from "./routes/shoppingRouter.js";
+import bodyParser from "body-parser";
+import cors from "cors";
+import express from "express";
+import http from "http";
+import { Server } from "socket.io";
+
+const app = express();
+const server = http.createServer(app); // create http server and pass express app as argument
+const io = new Server(server);
+
+let activeUsers = 0;
+
+io.on("connection", (socket) => {
+  activeUsers++;
+  io.emit("activeUsers", activeUsers);
+  socket.on("disconnect", () => {
+    activeUsers--;
+    io.emit("activeUsers", activeUsers);
+  });
+});
+
 mongoose
-  .connect('mongodb+srv://pro:DDwiYcBcxZDXEHDR@cluster0.iqfprtk.mongodb.net/WineryStore')
+  .connect(
+    "mongodb+srv://pro:DDwiYcBcxZDXEHDR@cluster0.iqfprtk.mongodb.net/WineryStore"
+  )
   .then(() => {
-    console.log('connected to db');
+    console.log("connected to db");
   })
   .catch((err) => {
     console.log(err.message);
   });
 
-const app = express();
-app.use('/api/seed', seedRouter);
-app.use('/api/products', productRouter);
+app.use("/api/seed", seedRouter);
+app.use("/api/products", productRouter);
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-app.use('/', shoppingRouter);
+app.use("/", shoppingRouter);
+
 const port = process.env.PORT || 5000;
-app.listen(port, () => {
+server.listen(port, () => {
   console.log(`serve at http://localhost:${port}`);
 });
--- frontend/src/components/ImageWithInfo.js
+++ frontend/src/components/ImageWithInfo.js
@@ -1,29 +1,28 @@
-import React, { useState } from 'react';
-import { Link, Route } from 'react-router-dom';
+import React, { useState } from "react";
+import { Link, Route } from "react-router-dom";
 import CatalogScreen from "../screens/CatalogScreen";
 
 function ImageWithInfo({ imageUrl, imageAlt, infoText, linkText, linkTo }) {
-    const [showInfo, setShowInfo] = useState(false);
-    <Route path={linkTo} element={<CatalogScreen />} />
-    return (
+  const [showInfo, setShowInfo] = useState(false);
+  return (
+    <div>
+      <img
+        className="card-img-top-home"
+        src={imageUrl}
+        alt={imageAlt}
+        onClick={() => setShowInfo(!showInfo)}
+      />
+      {showInfo && (
         <div>
-            <img className="card-img-top-home"
-                src={imageUrl}
-                alt={imageAlt}
-                onClick={() => setShowInfo(!showInfo)}
-            />
-            {showInfo && (
-                <div>
-                    <p className='para'>{infoText}
-                        <br></br>
-                        <Link to="/catalog">
-                            Search on our Catalog
-                        </Link>
-                    </p>
-                </div>
-            )}
+          <p className="para">
+            {infoText}
+            <br></br>
+            <Link to="/catalog">Search on our Catalog</Link>
+          </p>
         </div>
-    );
+      )}
+    </div>
+  );
 }
 
 export default ImageWithInfo;
